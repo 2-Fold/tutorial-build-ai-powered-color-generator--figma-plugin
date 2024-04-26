2317ebe76608023472123761f4733ec54cfc54c9
Lee Nelson <lee@2fold.io>
2024-04-26 13:44:32 -0700

    finished version

--- figma-plugin/code.ts
+++ figma-plugin/code.ts
@@ -1,8 +1,56 @@
-figma.on('run', async ({ parameters }: RunEvent) => {
+interface Color {
+  name: string;
+  value: string;
+}
+
+async function createColorPalette(colors: Color[]) {
+  const nodes: SceneNode[] = [];
+  colors.forEach(({ name, value }) => {
+    const rect = figma.createRectangle();
+    rect.name = name;
+    rect.cornerRadius = 24;
+    rect.fills = [{ type: 'SOLID', color: figma.util.rgb(value) }];
+    rect.x = nodes.length * 120;
+    rect.y = 0;
+    rect.resize(100, 100);
+    nodes.push(rect);
+  });
+
+  figma.currentPage.selection = nodes;
+  figma.viewport.scrollAndZoomIntoView(nodes);
+}
+
+async function fetchColors(colorsDescription: string) {
+  return fetch('http://localhost:3000', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ payload: colorsDescription }),
+  })
+    .then((response) => response.json())
+    .then((data) => data.choices[0].message.content)
+    .catch((error) => error);
+}
+
+async function parseData(data: string) {
   try {
-    const response = await fetch('http://localhost:3000');
-    console.log(await response.json());
+    const parsedData = JSON.parse(data);
+    return parsedData;
   } catch (error) {
-    console.error({ error });
+    console.error('Invalid JSON');
+    return null;
   }
+}
+
+figma.on('run', async ({ parameters }: RunEvent) => {
+  const colorsDescription = parameters?.['colorsDescription'].trim();
+  const colorArray = await fetchColors(colorsDescription);
+  const parsedColorArray = await parseData(colorArray);
+  if (!parsedColorArray) {
+    figma.closePlugin('Invalid JSON');
+    return;
+  }
+  await createColorPalette(parsedColorArray);
+  figma.closePlugin();
 });
--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,39 @@
 import express from 'express';
 import cors from 'cors';
+import OpenAI from 'openai';
 
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello World!' });
+app.use(cors(), express.json());
+
+app.post('/', (req, res) => {
+  openai.chat.completions
+    .create({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'system',
+          content:
+            'You will be provided with a description and your task is to pick colour names and values that match the description. Write your output in a json array with objects containing name and value keys. Needs to be valid JSON all keys and values should be wrapped in quotations. Do not include any delimiters. If no number of colours is specified, create 5 values.',
+        },
+        {
+          role: 'user',
+          content: req.body.payload,
+        },
+      ],
+      response_format: 'json' as
+        | OpenAI.Chat.Completions.ChatCompletionCreateParams.ResponseFormat
+        | undefined,
+      max_tokens: 256,
+      temperature: 1,
+    })
+    .then((response) => res.json(response))
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
 });
 
 app.listen(PORT, () => {
